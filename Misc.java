638b022f44f9a926bdcf2e83cbd26fa782219f2e
Calvin Xiao <228013@qq.com>
2016-10-22 22:50:39 +0800

    Update Misc.java

--- Misc.java
+++ Misc.java
@@ -2,3 +2,7 @@
 println(strcat(strcat(probeClass, "."), probeMethod));
 
 @OnMethod(clazz="+com.vip.demo.Filter", method="doFilter")
+
+@OnMethod(clazz="@javax.jws.WebService", method="@javax.jws.WebMethod")
+
+@OnMethod(clazz="java.net.ServerSocket", method="<init>")
